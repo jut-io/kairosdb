a0ddf1023e624c46db3b36c1b3b01ae71a575072
Greg Lee Coleman <gleeco@jut.io>
2014-04-21 10:58:40 -0700

    extend implementation of env vars at runtime
    Allow users to implement custom pathing
    via ENV vars, mainly from KAIROSDB_ENV_FILE.
    Changes retain prior setup as defaults.
    
    * exposes the following in setup:
     - KAIROSDB_ENV_FILE
     - KAIROSDB_CONF_DIR
     - KAIROSDB_CONF_FILE
     - KAIROSDB_LIB_DIR
     - KAIROSDB_LOG_DIR
     - KAIROSDB_LOG_FILE
     - KAIROSDB_PID_FILE
    
     * mkdir -p on log dir if does not exist
     * rename pid var for consistency
     * checks conf for *.xml files if ! -d conf/logging

--- src/scripts/kairosdb-env.sh
+++ src/scripts/kairosdb-env.sh
@@ -1,4 +1,15 @@
+# this file may live anywhere;
+# use KAIROSDB_ENV_FILE at startup to access it
 
+# Java options
 #JAVA_OPTS="$JAVA_OPTS -Xmx4G"  # Max heap size
 #JAVA_OPTS="$JAVA_OPTS -Xms4G"  # Min heap size
+
+# Kairosdb options (and defaults)
+# KAIROSDB_LIB_DIR=lib
+# KAIROSDB_LOG_DIR=log
+# KAIROSDB_CONF_DIR=conf
+# KAIROSDB_CONF_FILE=conf/kairosdb.properties
+# KAIROSDB_LOG_FILE=log/kairosdb.log
+# KAIROSDB_PID_FILE=/var/run/kairosdb.pid
 
--- src/scripts/kairosdb.sh
+++ src/scripts/kairosdb.sh
@@ -4,21 +4,22 @@
 KAIROSDB_BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$KAIROSDB_BIN_DIR/.."
 
-KAIROSDB_LIB_DIR="lib"
-KAIROSDB_LOG_DIR="log"
+KAIROSDB_ENV_FILE=${KAIROSDB_ENV_FILE:-"$KAIROSDB_BIN_DIR/kairosdb-env.sh"}
 
-if [ -f "$KAIROSDB_BIN_DIR/kairosdb-env.sh" ]; then
-	. "$KAIROSDB_BIN_DIR/kairosdb-env.sh"
+if [ -f "$KAIROSDB_ENV_FILE" ]; then
+	. $KAIROSDB_ENV_FILE
 fi
+
+KAIROSDB_CONF_DIR=${KAIROSDB_CONF_DIR:-"conf"}
+KAIROSDB_LOG_DIR=${KAIROSDB_LOG_DIR:-"log"}
+KAIROSDB_LIB_DIR=${KAIROSDB_LIB_DIR:-"lib"}
+KAIROSDB_PID_FILE=${KAIROSDB_PID_FILE:-"/var/run/kairosdb.pid"}
+KAIROSDB_CONF_FILE=${KAIROSDB_CONF_FILE:-"$KAIROSDB_CONF_DIR/kairosdb.properties"}
+KAIROSDB_LOG_FILE=${KAIROSDB_PID_FILE:-"$KAIROSDB_LOG_DIR/kairosdb.log"}
 
 if [ ! -d "$KAIROSDB_LOG_DIR" ]; then
-	mkdir "$KAIROSDB_LOG_DIR"
+	mkdir -p "$KAIROSDB_LOG_DIR"
 fi
-
-if [ "$KAIROS_PID_FILE" = "" ]; then
-	KAIROS_PID_FILE=/var/run/kairosdb.pid
-fi
-
 
 # Use JAVA_HOME if set, otherwise look for java in PATH
 if [ -n "$JAVA_HOME" ]; then
@@ -28,7 +29,11 @@ else
 fi
 
 # Load up the classpath
-CLASSPATH="conf/logging"
+if [ -d "$KAIROSDB_CONF_DIR/logging" ]; then
+    CLASSPATH="$KAIROSDB_CONF_DIR/logging"
+elif [ "$KAIROSDB_CONF_DIR/*xml" != "" ]; then
+    CLASSPATH="$KAIROSDB_CONF_DIR"
+fi
 for jar in $KAIROSDB_LIB_DIR/*.jar; do
 	CLASSPATH="$CLASSPATH:$jar"
 done
@@ -37,26 +42,26 @@ done
 
 if [ "$1" = "run" ] ; then
 	shift
-	exec "$JAVA" $JAVA_OPTS -cp $CLASSPATH org.kairosdb.core.Main -c run -p conf/kairosdb.properties
+	exec "$JAVA" $JAVA_OPTS -cp $CLASSPATH org.kairosdb.core.Main -c run -p $KAIROSDB_CONF_FILE
 elif [ "$1" = "start" ] ; then
 	shift
 	exec "$JAVA" $JAVA_OPTS -cp $CLASSPATH org.kairosdb.core.Main \
-		-c start -p conf/kairosdb.properties >> "$KAIROSDB_LOG_DIR/kairosdb.log" 2>&1 &
-	echo $! > "$KAIROS_PID_FILE"
+		-c start -p $KAIROSDB_CONF_FILE >> $KAIROSDB_LOG_FILE 2>&1 &
+	echo $! > "$KAIROSDB_PID_FILE"
 elif [ "$1" = "stop" ] ; then
 	shift
-	kill `cat $KAIROS_PID_FILE` > /dev/null 2>&1
-	while kill -0 `cat $KAIROS_PID_FILE` > /dev/null 2>&1; do
+	kill `cat $KAIROSDB_PID_FILE` > /dev/null 2>&1
+	while kill -0 `cat $KAIROSDB_PID_FILE` > /dev/null 2>&1; do
 		echo -n "."
 		sleep 1;
 	done
-	rm $KAIROS_PID_FILE
+	rm $KAIROSDB_PID_FILE
 elif [ "$1" = "export" ] ; then
 	shift
-	exec "$JAVA" $JAVA_OPTS -cp $CLASSPATH org.kairosdb.core.Main -c export -p conf/kairosdb.properties $*
+	exec "$JAVA" $JAVA_OPTS -cp $CLASSPATH org.kairosdb.core.Main -c export -p $KAIROSDB_CONF_FILE $*
 elif [ "$1" = "import" ] ; then
 	shift
-	exec "$JAVA" $JAVA_OPTS -cp $CLASSPATH org.kairosdb.core.Main -c import -p conf/kairosdb.properties $*
+	exec "$JAVA" $JAVA_OPTS -cp $CLASSPATH org.kairosdb.core.Main -c import -p $KAIROSDB_CONF_FILE $*
 else
 	echo "Unrecognized command."
 	exit 1
